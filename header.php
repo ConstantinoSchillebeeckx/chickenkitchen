a2df9bf2363be72dce10f70e3a37fe19134fc5b1
Constantino Schillebeeckx <titous@gmail.com>
2017-04-26 05:26:33 -0700

    submodule

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="http://www.meepmoop.com/chickenkitchen/js/table.js"></script>
         <script type="text/javascript" src="http://www.meepmoop.com/chickenkitchen/meowcow/js/plugin.js"></script>
-        <script type="text/javascript" src="http://www.meepmoop.com/chickenkitchen/js/jquery.TableCSVExport.js"></script>
+        <script type="text/javascript" src="http://www.meepmoop.com/chickenkitchen/TableCSVExport/jquery.TableCSVExport.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.query-builder/2.4.1/js/query-builder.standalone.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js" integrity="sha256-urCxMaTtyuE8UK5XeVYuQbm/MhnXflqZ/B9AOkyTguo=" crossorigin="anonymous"></script>
         <script type="text/javascript">var DEBUG = <?php echo DEBUG; ?></script>
