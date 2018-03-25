7c55d2c57ad5c27c9ff72d688bb06400a124b582
Constantino Schillebeeckx <ConstantinoSchillebeeckx@users.noreply.github.com>
2018-03-25 13:39:01 -0500

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+    // DB ACCESS
+    define("HOST_DB", "EDIT THIS");
+    define("NAME_DB", "EDIT THIS");
+    define("USER_DB", "EDIT THIS");
+    define("PASS_DB", "EDIT THIS");
+
+    define("ADD_TABLE_URL_PATH", 'add-table');
+    define("BATCH_TABLE_URL_PATH", 'batch-table');
+    define("VIEW_TABLE_URL_PATH", 'view-table');
+    define("DEBUG", true);
+
+    // USER INFO
+    // used for debug ATM
+    define("ACCT", ""); // company name
+    define("USER_NAME", "Connie");
+    define("USER_ROLE", 'administrator'); // see https://github.com/ConstantinoSchillebeeckx/chickenkitchen/issues/12
+?>
